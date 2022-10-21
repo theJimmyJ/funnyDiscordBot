4500d66133916d09acb7fdf861ed2f640632d77d
Jim Jones <112525347+theJimmyJ@users.noreply.github.com>
2022-10-20 21:26:00 -0400

    Update testArgs.ts
    Command was returning index of the args, not the actual user input

--- src/commands/testArgs.ts
+++ src/commands/testArgs.ts
@@ -4,7 +4,7 @@ import {Command} from "utils/types/Command";
 export const TestArgsCommand: Command = {
     run: (client, msg, args) => {
         for (const arg in args) {
-            msg.channel.send(arg)
+            msg.channel.send(args[arg])
         }
     },
     name: "testArgs",
